6d7a23a1000a9633d4e3c9d4340657e68f96dfef
zaki358 <t.yamazaki.1129@gmail.com>
2023-02-26 15:40:28 +0900

    コミット

--- build/blocks/block-one/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '63bec48736358f1f7b1d');
--- build/blocks/text/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2270c7163640aded2f99');
